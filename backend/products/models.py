0b53464bf0b78df2935189c01edb46bd8bee3df1
Alfrederick888 <al-gilm@yandex.ru>
2023-05-20 12:58:17 +0500

    first commit

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Products(models.Model):
+    name = models.CharField(max_length=255, verbose_name='Наименование товара')
+    price = models.CharField(max_length=255, verbose_name='Цена')
+    size = models.CharField(max_length=255, verbose_name='Размер', blank=True)
+    picture = models.ImageField(upload_to='picture/%Y/%m/%d/', verbose_name='Фото')
+    description = models.TextField(blank=True, verbose_name='Описание товара')
+    quantity = models.IntegerField(verbose_name='Количество в наличии')
+    is_published = models.BooleanField(default=True, verbose_name='Публикация')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('card', kwargs={'card_id': self.pk})
+
+    class Meta:
+        verbose_name = 'Товары'
+        verbose_name_plural = 'Товары'
+        ordering = ('name', 'price')
+
+
+class Status(models.Model):
+    status_name = models.CharField(max_length=255, verbose_name='Название статуса')
+
+    def __str__(self):
+        return self.status_name
+
+    class Meta:
+        verbose_name = 'Статус'
+        verbose_name_plural = 'Статусы'
+
+
+class Orders(models.Model):
+    data = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=200, verbose_name='Имя')
+    phone = models.CharField(max_length=200, verbose_name='Телефон')
+    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True, blank=True, verbose_name='Статус')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
--- backend/products/views.py
+++ backend/products/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import *
 
-from django.http import HttpResponse
 
-def index(request):    
-    return HttpResponse('Главная страница')
+def main_view(request):
+    cards = Products.objects.all()
+    context = {
+        'cards': cards,
+    }
+    return render(request, 'products/base.html', context=context)
+
+
+def show_card(request, card_id):
+    card = get_object_or_404(Products, pk=card_id)
+    context = {
+        'card': card,
+    }
+    return render(request, 'products/card_page.html', context=context)
+
+
+def buy_product(request):
+    card = Products.objects.all()
+    context = {
+        'card': card,
+    }
+    return render(request, 'products/buy_page.html', context=context)
